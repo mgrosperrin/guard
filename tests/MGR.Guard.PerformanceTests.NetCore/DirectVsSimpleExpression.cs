5a8ce52db8188430ba5f51e17f6c3e819a11bcdf
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2018-08-08 10:20:21 +0200

    Add benchmark project to show the difference between the classic all and the expression based one

---
+++ tests/MGR.Guard.PerformanceTests.NetCore/DirectVsSimpleExpression.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MGR.Guard.PerformanceTests.NetCore
+{
+    [CoreJob]
+    public class DirectVsSimpleExpression
+    {
+        private readonly object _obj;
+
+        public DirectVsSimpleExpression()
+        {
+            _obj = new object();
+        }
+
+        [Benchmark]
+        public void Direct()
+        {
+            Direct(_obj);
+        }
+
+        private void Direct(object obj)
+        {
+            Guard.IsNotNull(obj, nameof(obj));
+        }
+
+        [Benchmark]
+        public void SimpleExpression()
+        {
+            SimpleExpression(_obj);
+        }
+
+        private void SimpleExpression(object obj)
+        {
+            Guard.IsNotNull(() => obj);
+        }
+    }
+}
